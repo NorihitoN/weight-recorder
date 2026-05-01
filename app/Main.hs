85e52fb9d53070474e1c6f3035e152450a8faa76
NorihitoN <norihito0626@gmail.com>
2026-05-01 11:59:38 +0900

    Added Router.hs

--- app/Main.hs
+++ app/Main.hs
@@ -3,19 +3,10 @@
 module Main where
 
 import Database.HDBC.Sqlite3 (connectSqlite3)
-import Web.Action.Login (getLogin, postLogin)
-import Web.Action.NewRecord (getNewRecord, getRecords, postRecords)
-import Web.Action.Register (getRegister, postRegister)
+import qualified Web.Router as Router
 import Web.Scotty
 
 main :: IO ()
 main = do
   conn <- connectSqlite3 "weight.db"
-  scotty 8080 $ do
-    get  "/register"   getRegister
-    post "/register"   (postRegister conn)
-    get  "/login"      getLogin
-    post "/login"      (postLogin conn)
-    get  "/records"    getRecords
-    get  "/records/new" getNewRecord
-    post "/records"    (postRecords conn)
+  scotty 8080 $ Router.routes conn
---
+++ src/Web/Router.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Web.Router (routes) where
+
+import Database.HDBC.Sqlite3 (Connection)
+import Web.Action.Login (getLogin, postLogin)
+import Web.Action.NewRecord (getNewRecord, getRecords, postRecords)
+import Web.Action.Register (getRegister, postRegister)
+import Web.Scotty
+
+routes :: Connection -> ScottyM ()
+routes conn = do
+  get  "/register"    getRegister
+  post "/register"    (postRegister conn)
+  get  "/login"       getLogin
+  post "/login"       (postLogin conn)
+  get  "/records"     getRecords
+  get  "/records/new" getNewRecord
+  post "/records"     (postRecords conn)
